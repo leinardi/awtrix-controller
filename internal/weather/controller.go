0ca7bd2243a8a468e126a246adf540ada406103e
Roberto Leinardi <leinardi@gmail.com>
2026-03-05 22:23:15 +0100

    Better weather icons

--- internal/weather/controller.go
+++ internal/weather/controller.go
@@ -41,12 +41,12 @@ const (
 
 	severityColor = "#FFFF00" // yellow text for severe-level notifications
 
-	iconThunderstorm   = "63084"
-	iconFreezingPrecip = "60934"
-	iconFrostRisk      = "43125"
-	iconHeavyRain      = "22366"
+	iconThunderstorm   = "49299"
+	iconFreezingPrecip = "49301"
+	iconFrostRisk      = "4807"
+	iconHeavyRain      = "49300"
 	iconStrongGusts    = "3363"
-	iconSnow           = "63083"
+	iconSnow           = "2289"
 	iconFog            = "17056"
 )
 
